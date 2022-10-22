fed9a7d220229c739f7c34c3ea60b60d55bdb249
devguusta <gustavodev60@gmail.com>
2022-10-22 11:47:21 -0300

    create guard and strategy jwt

---
+++ src/auth/strategy/jwt_strategy.ts
@@ -0,0 +1,43 @@
+import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { PassportStrategy } from '@nestjs/passport';
+import { InjectRepository } from '@nestjs/typeorm';
+import {
+  ExtractJwt,
+  Strategy,
+} from 'passport-jwt';
+import { User } from 'src/entity/user.entity';
+import { Repository } from 'typeorm';
+
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(
+  Strategy,
+  'jwt',
+) {
+  constructor(
+    config: ConfigService,
+    @InjectRepository(User)
+    private userRepository: Repository<User>,
+  ) {
+    super({
+      jwtFromRequest:
+        ExtractJwt.fromAuthHeaderAsBearerToken(),
+      secretOrKey: config.get('JWT_SECRET'),
+    });
+  }
+
+  async validate(payload: {
+    sub: number;
+    email: string;
+  }) {
+    const user =
+      await this.userRepository.findOne({
+        where: {
+          id: payload.sub,
+        }
+      });
+    delete user.password;
+    return user;
+  }
+}
