e1fa0168717d74634f5b89561d5a52d81b4b5ab3
devguusta <gustavodev60@gmail.com>
2023-01-23 19:20:35 -0300

    preparação nba allstar modulo

---
+++ src/entity/nba_allstar_voting.entity.ts
@@ -0,0 +1,21 @@
+import { Column, Entity, JoinColumn, OneToMany, OneToOne, PrimaryGeneratedColumn } from "typeorm";
+import { NbaPlayer } from "./nba_player.entity";
+import { User } from "./user.entity";
+
+@Entity()
+export class NbaAllStarVoting{
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @Column()
+    votes: number;
+
+    @OneToMany(() => NbaPlayer, (player) => player.idPlayer)
+    player: NbaPlayer[];
+
+    @OneToOne(()=> User)
+    @JoinColumn()
+    user: User;
+
+
+}
